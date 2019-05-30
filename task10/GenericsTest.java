b84e39f0cb3ad2fb5c9b83a6c5d35f9a0505ba74
AndLvovSky <viktorilvovskyi@ukr.net>
2019-05-30 10:50:59 +0300

    Add second part of task 10

--- task10/GenericsTest.java
+++ task10/GenericsTest.java
@@ -1,6 +1,4 @@
-import java.util.Collection;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class GenericsTest {
 
@@ -11,6 +9,16 @@ public class GenericsTest {
 		}
 	}
 
+	public static <T> void copyAll(
+		Collection<? extends T> from, Collection<? super T> to) {
+		to.clear();
+		Iterator<? extends T> it = from.iterator();
+		while (it.hasNext()) {
+			T cur = it.next();
+			to.add(cur);
+		}
+	}
+
 	private static void testArrToColl() {
 		Integer[] arr = new Integer[]{1, 2, 3, 4, 5};
 		ArrayList<Integer> list = new ArrayList<>();
@@ -18,8 +26,34 @@ public class GenericsTest {
 		System.out.println(Arrays.toString(list.toArray()));
 	}
 
+	private static class A {
+		public int n;
+		public A(int n) {
+			this.n = n;
+		}
+		public String toString() {
+			return String.valueOf(n);
+		}
+	}
+	private static class B extends A {
+		public B(int n) {
+			super(n);
+		}
+	}
+
+	private static void testCopyAll() {
+		ArrayList<B> list1 = new ArrayList<>();
+		list1.add(new B(1));
+		list1.add(new B(2));
+		list1.add(new B(3));
+		ArrayList<A> list2 = new ArrayList<>();
+		copyAll(list1, list2);
+		System.out.println(Arrays.toString(list2.toArray()));
+	}
+
 	public static void main(String[] args) {
 		testArrToColl();
+		testCopyAll();
 	}
 
 }
